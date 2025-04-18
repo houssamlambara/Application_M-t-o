e634f8e2736dd09162bbcc1ebc85b99677136591
houssamlambara <lambarahoussam@gmail.com>
2025-04-18 17:13:41 +0100

    feat: add router configuration and assets

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <WeatherSearch />
+    <WeatherDisplay />
+  </div>
+</template>
+
+<script setup>
+import WeatherSearch from '../components/WeatherSearch.vue'
+import WeatherDisplay from '../components/WeatherDisplay.vue'
+</script> 
